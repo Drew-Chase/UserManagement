53d000c522926c7a1996585470dcbee8aec0878e
drew-chase <dcmanproductions@gmail.com>
2023-12-14 16:25:44 -0500

    Add max-height to user list container and update user list header

--- pages/dashboard/dashboard.php
+++ pages/dashboard/dashboard.php
@@ -20,6 +20,7 @@
         padding: 0.5rem;
         border-radius: 0.5rem;
         height: fit-content;
+        max-height: 66px;
         transition: background 100ms;
     }
 
@@ -29,16 +30,26 @@
     }
 </style>
 
-<div class="col fill" style="padding: 1rem">
-    <h1 class="page-title">Dashboard</h1>
-
-    <div class="col fill" id="user-list">
-        <div class="list-header row center vertical">
+<div class="col fill" id="user-list">
+    <div class="list-header col center vertical island">
+        <div class="row fill center vertical">
             <h2 class="fill">Users</h2>
-            <button class="secondary" id="add-user" onclick="loadPage('new-user')">Add User</button>
+            <button onclick="loadPage('new-user')" title="Add a new user"><i class="fa-solid fa-plus"></i> Add New User</button>
         </div>
-        <div class="list">
+        <div class="list-header row center vertical fill" style="margin-top: 1rem">
+            <div class="search-input" style="min-width:fit-content; width: 100%;">
+                <input type="search" name="" id="" placeholder="Search">
+            </div>
+            <select name="" id="" style="min-width:fit-content; width: 15%; margin-right: 1rem;">
+                <option value="" disabled selected>Filter by Permissions</option>
+                <option value="">All</option>
+                <option value="">Admin</option>
+                <option value="">User</option>
+            </select>
+            <button onclick="editUser()" class="secondary" title="Filter the results" style="height: 100%;"><i class="fa-solid fa-magnifying-glass"></i>Search</button>
         </div>
+    </div>
+    <div class="list island">
     </div>
 </div>
 
@@ -53,11 +64,34 @@
                         <h3>${user.username}</h3>
                     </div>
                     <div class="row">
-                        <button onclick="editUser(${user.id})" title="Modify the user"><i class="fa-solid fa-pen"></i></button>
-                        <button class="secondary" onclick="deleteUser(${user.id})" title="Delete the user"><i class="fa-solid fa-trash"></i></button>
+                        <button onclick="editUser('${user.id}')" title="Modify the user"><i class="fa-solid fa-pen"></i></button>
+                        <button class="secondary" onclick="deleteUser('${user.id}')" title="Delete the user"><i class="fa-solid fa-trash"></i></button>
                     </div>
                 </div>
-            `);
+                `);
         });
     })();
+
+    function editUser(id) {
+        loadPage("edit-user", {
+            id
+        });
+    }
+
+    function deleteUser(id) {
+        $.ajax(`/api/auth.php?id=${id}`, {
+            method: "DELETE",
+            success: e => {
+                if(e.error) {
+                    loadAlert("error", `An error occurred while deleting the user<br>${e.error}`);
+                    return;
+                }
+                loadPage("");
+            },
+            error: error => {
+                console.error(error)
+                loadAlert("error", `An error occurred while deleting the user<br>${error.responseJSON.error}`);
+            }
+        });
+    }
 </script>
