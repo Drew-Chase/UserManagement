3e123745ca5910fe787f6ebfc0015a667e962c48
drew-chase <dcmanproductions@gmail.com>
2023-12-14 16:25:58 -0500

    Add login form animation and handle login response

--- pages/login.php
+++ pages/login.php
@@ -54,6 +54,38 @@
         animation-duration: .7s;
     }
 
+    #login-form.close h1 {
+        opacity: 1;
+        animation-name: closePage;
+        animation-fill-mode: forwards;
+        animation-duration: .7s;
+        animation-delay: .4s;
+    }
+
+    #login-form.close form>*:nth-child(1) {
+        opacity: 1;
+        animation-name: closePage;
+        animation-fill-mode: forwards;
+        animation-duration: .7s;
+        animation-delay: .3s;
+    }
+
+    #login-form.close form>*:nth-child(2) {
+        opacity: 1;
+        animation-name: closePage;
+        animation-fill-mode: forwards;
+        animation-duration: .7s;
+        animation-delay: .2s;
+    }
+
+    #login-form.close form>*:nth-child(3) {
+        opacity: 1;
+        animation-name: closePage;
+        animation-fill-mode: forwards;
+        animation-duration: .7s;
+        animation-delay: 0s;
+    }
+
     @keyframes loadPage {
         0% {
             transform: translateY(100px);
@@ -63,6 +95,18 @@
         100% {
             transform: translateY(0);
             opacity: 1;
+        }
+    }
+
+    @keyframes closePage {
+        0% {
+            transform: translateY(0);
+            opacity: 1;
+        }
+
+        100% {
+            transform: translateY(100px);
+            opacity: 0;
         }
     }
 </style>
@@ -88,12 +132,28 @@
     const form = $("form");
     const button = $("button");
 
+    $(auth).on("logged-in", () => {
+        $("#login-form").addClass("close");
+        $("main").load("/pages/dashboard.php");
+        setTimeout(() => {
+        }, 1200);
+    });
+
+    auth.loginWithToken();
+
     form.on("submit", async () => {
+
         button.attr("disabled", true);
         button.html("Logging in...");
         let formdata = form.serializeArray();
         let username = formdata[0]["value"];
         let password = formdata[1]["value"];
         let response = await auth.login(username, password)
+
+        if (!response.success) {
+            button.html("Login");
+            button.attr("disabled", false);
+            popup.alert("Login Failed", response.message);
+        }
     });
 </script>
