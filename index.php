f8a18e44aea7357888ecc6a681dde91491b79063
drew-chase <dcmanproductions@gmail.com>
2023-12-14 08:10:08 -0500

    Add index.php file with initial HTML structure and authentication logic

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Management - Marden's Surplus & Salvage | Maine's Best Bargain Store</title>
+    <link rel="shortcut icon" href="https://www.mardens.com/wp-content/uploads/2019/03/cropped-Mardens-Favicon-1-192x192.png" type="image/x-icon">
+
+    <!-- Page Stylings -->
+    <link rel="stylesheet" href="/assets/css/main.min.css">
+    <link rel="stylesheet" href="/assets/css/inputs.min.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/assets/lib/fontawesome/css/all.min.css">
+
+    <!-- jQuery -->
+    <script src="/assets/lib/jquery.min.js"></script>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <script src="/assets/js/authentication.js"></script>
+
+</head>
+
+<body>
+    <main>
+    </main>
+
+    <script>
+        const auth = new Authentication();
+        if(!auth.isLoggedIn()) {
+            $("main").load("/pages/login.php");
+        }
+
+        $(auth).on("logged-in", () => {
+            $("main").load("/pages/dashboard.php");
+        });
+
+    </script>
+</body>
+
+</html>
