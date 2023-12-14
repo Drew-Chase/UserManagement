c2831ea815a6120be701e141e96155af8d71770a
drew-chase <dcmanproductions@gmail.com>
2023-12-14 11:36:11 -0500

    Refactor dashboard.php layout and add scroll functionality

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -2,7 +2,6 @@
     .page-content {
         display: flex;
         justify-content: center;
-        align-items: center;
         margin-top: 2rem;
         height: calc(100vh - 4rem);
         width: calc(100% - 164px);
@@ -10,7 +9,19 @@
         border-radius: 5px;
         min-width: 500px;
         min-height: 500px;
+        overflow: scroll;
     }
+
+    
+    .page-title {
+        margin: 0;
+        margin-bottom: 1rem;
+        font-size: 120px;
+        opacity: 0.2;
+        text-transform: uppercase;
+        font-weight: 900;
+    }
+
 </style>
 
 <div class="row" style="flex-wrap:nowrap;">
@@ -27,12 +38,15 @@
         loadPage(window.location.pathname.replace("/", ""));
         $(document).on("page-change", (_, page) => loadPage(page))
 
-        function loadPage(page) {
-            $(".nav-item").removeClass("selected");
-            $(`.nav-item[page="${page}"]`).addClass("selected");
 
-            page = page == "" ? "dashboard" : page;
-            $(".page-content").load(`/pages/dashboard/${page}.php`);
-        }
-    })()
+    })();
+
+    function loadPage(page) {
+        window.history.pushState({}, page, `/${page}`);
+        $(".nav-item").removeClass("selected");
+        $(`.nav-item[page="${page}"]`).addClass("selected");
+
+        page = page == "" ? "dashboard" : page;
+        $(".page-content").load(`/pages/dashboard/${page}.php`);
+    }
 </script>
