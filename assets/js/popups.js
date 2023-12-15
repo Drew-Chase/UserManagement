b154b2ebc9304504d71bd0ca9c5c05575af9b2ed
drew-chase <dcmanproductions@gmail.com>
2023-12-15 12:40:11 -0500

    Refactor loadAlert function to improve readability and add support for HTML content

--- assets/js/popups.js
+++ assets/js/popups.js
@@ -3,26 +3,29 @@
  * 
  * @param {string} title The title of the alert box
  * @param {string} message The alert message
- * @param {Function:void} onOk When the okay button is clicked
- * @param {Function:void} onCancel WHen the cancel button is clicked
+ * @param {(e: HTMLElement) => void} [onOk=(e) => {}] The function to run when the ok button is clicked
+ * @param {(e: HTMLElement) => void} [onCancel=(e) => {}] The function to run when the cancel button is clicked
+ * @returns {Promise<void>}
  */
-async function loadAlert(title, message, onOk = () => {}, onCancel = () => {}) {
+async function loadAlert(title, message, onOk = (e) => {}, onCancel = (e) => {}) {
     let alert = $(await $.get("/components/alert.php"));
     alert.find(".title").html(title);
-    alert.find(".message").html(message);
+    alert.find(".message").html(`<p>${message}</p>`);
     alert.find("#popup-ok").click(() => {
         alert.addClass("close");
         setTimeout(() => {
             alert.remove();
         }, 500);
-        onOk();
+        onOk($(alert.find(".message").html()));
     });
     alert.find("#popup-cancel").click(() => {
         alert.addClass("close");
         setTimeout(() => {
             alert.remove();
         }, 500);
-        onCancel();
+        onCancel($(alert.find(".message").html()));
     });
     $("body").append(alert);
+
+    $(document).trigger('page-loaded');
 }
